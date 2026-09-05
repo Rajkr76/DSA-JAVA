b0acf209212545dcb227041dac62e6d3e71e383a
Raj kumar <rajk76986@gmail.com>
2026-09-06 02:28:42 +0530

    0268-missing-number

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,17 +1,13 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        
-        int n = nums.length+1;
-        for(int i =0;i<n;i++){
-            boolean found =false;
-            for(int j=0;j<n-1;j++){
-                if(nums[j]==i){
-                    found = true;
-                    break;
-                }
-                }
-                if(!found){
-                    return i;
+        int n = nums.length;
+        int[] hash = new int[n+1];
+        for(int i=0;i<n;i++){
+            hash[nums[i]]=1;
+        }
+        for(int i =0;i<n+1;i++){
+            if(hash[i]==0){
+                return i;
             }
         }
         return -1;
