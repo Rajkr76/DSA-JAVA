8f86021f82229fb07a694a7831681d69e7606131
Raj kumar <rajk76986@gmail.com>
2026-09-06 02:32:12 +0530

    0268-missing-number

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,3 +1,4 @@
+//better approach 
 class Solution {
     public int missingNumber(int[] nums) {
         int n = nums.length;
@@ -13,3 +14,25 @@ class Solution {
         return -1;
     }
 }
+
+// brute force
+// class Solution {
+//     public int missingNumber(int[] nums) {
+//         int n = nums.length+1;
+
+//         for(int i =0 ; i<n; i++){
+//             boolean found = false ;
+//             for( int j = 0 ; j < n-1;j++){
+//                 if(nums[j]==i){
+//                     found = true;
+//                     break;
+//                 }
+              
+//             }
+//             if(!found){
+//                 return i;
+//             }
+//         }
+//        return -1;
+//     }
+// }
