ee26a55122af97556ab157bfb403523c99893b62
Raj kumar <rajk76986@gmail.com>
2026-09-06 23:59:48 +0530

    0136-single-number

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,20 +1,41 @@
-// brute force approach 
-
+//Better approach
+import java.util.HashMap;
+import java.util.Map;
 class Solution {
     public int singleNumber(int[] nums) {
         int n = nums.length;
+         HashMap<Integer,Integer> map = new HashMap<>();
         for(int i =0;i<n;i++){
-            int count = 0;
-            int numb = nums[i];
-            for(int j =0; j<n;j++){
-                if(nums[j]==numb){
-                    count++;
-                }
-            }
-            if(count ==1){
-                return numb;
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        }
+        for(Map.Entry<Integer,Integer> entry:map.entrySet()){
+            if(entry.getValue()==1){
+                return entry.getKey();
             }
         }
         return -1;
     }
-}
+       
+ }
+
+
+// brute force approach 
+
+// class Solution {
+//     public int singleNumber(int[] nums) {
+//         int n = nums.length;
+//         for(int i =0;i<n;i++){
+//             int count = 0;
+//             int numb = nums[i];
+//             for(int j =0; j<n;j++){
+//                 if(nums[j]==numb){
+//                     count++;
+//                 }
+//             }
+//             if(count ==1){
+//                 return numb;
+//             }
+//         }
+//         return -1;
+//     }
+// }
